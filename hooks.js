4e862e2bbcdf989c95829a0990e2108fffdda20a
Bob Luursema <bob.luursema@nn.nl>
2019-01-11 14:47:34 +0100

    Play nicer with feature sets that don't always need TestCafé.

--- hooks.js
+++ hooks.js
@@ -1,14 +1,12 @@
 const testControllerHolder = require('./testControllerHolder');
-const runner = require('./runner')
+const Runner = require('./runner')
 
 module.exports = {
-    /* Create the test.js file with all the fixtures and tests, and run it (returns a promise that resolves in the `after` hook) */
-    async beforeAll(){
-        runner.createTestCafeScript()
-        runner.run()
-    },
     /* Wait for TestCafé to boot up and return the TestCafé controller */
-    before(){
+    before(data){
+        this.runner = new Runner()
+        this.runner.createTestCafeScript(data.pickle.name)
+        this.runner.run()
         return this.waitForTestController.then(function(t) {
             return t.maximizeWindow()
         });
@@ -16,10 +14,8 @@ module.exports = {
     /* Resolve the TestCafé controller promise, this ends the test for TestCafé */
     after(){
         testControllerHolder.free()
-    },
-    /* Close TestCafé after all the tests have finished */
-    afterAll(){
         let intervalId = null
+        let runner = this.runner
 
         function waitForTestCafe() {
             intervalId = setInterval(checkLastResponse, 500)
@@ -33,5 +29,5 @@ module.exports = {
         }
 
         waitForTestCafe()
-    }
+    },
 }
--- index.js
+++ index.js
@@ -8,10 +8,8 @@ module.exports = function(cucumber) {
     return {
         /* Run the hooks that start and stop TestCafé for Cucumber */
         setupHooks: function(){
-            cucumber.BeforeAll(hooks.beforeAll)
-            cucumber.Before(hooks.before)
-            cucumber.After(hooks.after)
-            cucumber.AfterAll(hooks.afterAll)
+            cucumber.Before({tags: '@testcafe'}, hooks.before)
+            cucumber.After({tags: '@testcafe'}, hooks.after)
             if(process.env.TESTCAFE_DEBUG){
                 cucumber.setDefaultTimeout(-1) // Do not timeout while debugging
             }
@@ -19,16 +17,13 @@ module.exports = function(cucumber) {
                 cucumber.setDefaultTimeout(60000)
             }
         },
-        /* Exposes TestCafé to the hooks and step definitions */
-        setupWorld: function(setupClasses){
-            if(setupClasses) global.setupClasses = setupClasses
-            cucumber.setWorldConstructor(pickleCafeWorld)
-        },
-        /* Wrap the Cucumber functions to make the TestCafé reporting work */
-        Given: function(pattern, fn){cucumber.defineStep(pattern, wrapper(fn))},
-        When: function(pattern, fn){cucumber.defineStep(pattern, wrapper(fn))},
-        Then: function(pattern, fn){cucumber.defineStep(pattern, wrapper(fn))},
+        /* Wrap the Cucumber functions to make the TestCafé reporting and debugging work */
+        GivenTestcafe: function(pattern, fn){cucumber.defineStep(pattern, wrapper(fn))},
+        WhenTestcafe: function(pattern, fn){cucumber.defineStep(pattern, wrapper(fn))},
+        ThenTestcafe: function(pattern, fn){cucumber.defineStep(pattern, wrapper(fn))},
+        defineStepTestCafe: function(pattern, fn){cucumber.defineStep(pattern, wrapper(fn))},
         BoundTestRunPage,
+        pickleCafeWorld,
         testcafe
     }
 }
