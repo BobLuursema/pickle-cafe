853dfa2585768a7833de2d91d32bf409dd472c88
Bob Luursema <git@localhost>
2019-08-09 13:31:28 +0200

    Update dependencies, prettier style guide

--- runner.js
+++ runner.js
@@ -1,61 +1,69 @@
-const createTestCafe = require('testcafe');
-const { writeFileSync, createWriteStream, unlinkSync } = require('fs');
-const debug = require('debug')('pickle-cafe:runner')
-const generateUUID = function(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,generateUUID)}
+const createTestCafe = require("testcafe");
+const { writeFileSync, createWriteStream, unlinkSync } = require("fs");
+const debug = require("debug")("pickle-cafe:runner");
+const generateUUID = function(a) {
+  return a
+    ? (a ^ ((Math.random() * 16) >> (a / 4))).toString(16)
+    : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, generateUUID);
+};
 
 /* Manage the TestCafé server */
 class Runner {
-    constructor(){
-        this.scenarioCount = 0
-        this.testcafe = null
-        this.testFile = null
-        this.options = {
-            browsers: process.env.TESTCAFE_BROWSERS ? process.env.TESTCAFE_BROWSERS : 'chrome',
-            debugOnFail: process.env.TESTCAFE_DEBUG === '1' ? true : false,
-            proxy: process.env.TESTCAFE_PROXY ? process.env.TESTCAFE_PROXY : null,
-            report_folder: process.env.TESTCAFE_REPORT ? process.env.TESTCAFE_REPORT : 'reports',
-        }
-    }
-    
-    /* Dynamically create the test.js file by parsing all the feature files inside the features folder */
-    createTestCafeScript(testname){
-        debug('create testscript')
-        this.testFile = `test_${generateUUID()}.js`
-        let importStatement = 'import testControllerHolder from "./node_modules/pickle-cafe/testControllerHolder"\n\n'
-        let testCode = ''
-        testCode += `fixture("${testname}")\n`
-        testCode += `test("${testname}", testControllerHolder.capture)\n`
-        writeFileSync(this.testFile, importStatement + testCode)
-    }
-    
-    /* Create the TestCafé runner */
-    run(){
-        debug(`start runner from ${this.testFile}`)
-        createTestCafe()
-        .then(testcafe => {
-            this.testcafe = testcafe;
-            const runner = testcafe.createRunner();
-            if(this.options.proxy){
-                runner.useProxy(this.options.proxy)
-            }
-            let runOptions = {
-                debugOnFail: this.options.debugOnFail,
-                skipJsErrors: true,
-            }
-            return runner
-                .src(`./${this.testFile}`)
-                .screenshots(`${this.options.report_folder}/screenshots/`, true)
-                .browsers(this.options.browsers)
-                .reporter('spec', `${this.options.report_folder}/report.txt`)
-                .run(runOptions)
-        })
-    }
+  constructor() {
+    this.scenarioCount = 0;
+    this.testcafe = null;
+    this.testFile = null;
+    this.options = {
+      browsers: process.env.TESTCAFE_BROWSERS
+        ? process.env.TESTCAFE_BROWSERS
+        : "chrome",
+      debugOnFail: process.env.TESTCAFE_DEBUG === "1" ? true : false,
+      proxy: process.env.TESTCAFE_PROXY ? process.env.TESTCAFE_PROXY : null,
+      report_folder: process.env.TESTCAFE_REPORT
+        ? process.env.TESTCAFE_REPORT
+        : "reports"
+    };
+  }
 
-    /* Close the stream, file and browser/server */
-    close(){
-        debug('close testcafe, close report stream, unlink file')
-        this.testcafe.close()
-        unlinkSync(`./${this.testFile}`)
-    }
+  /* Dynamically create the test.js file by parsing all the feature files inside the features folder */
+  createTestCafeScript(testname) {
+    debug("create testscript");
+    this.testFile = `test_${generateUUID()}.js`;
+    let importStatement =
+      'import testControllerHolder from "./node_modules/pickle-cafe/testControllerHolder"\n\n';
+    let testCode = "";
+    testCode += `fixture("${testname}")\n`;
+    testCode += `test("${testname}", testControllerHolder.capture)\n`;
+    writeFileSync(this.testFile, importStatement + testCode);
+  }
+
+  /* Create the TestCafé runner */
+  run() {
+    debug(`start runner from ${this.testFile}`);
+    createTestCafe("localhost").then(testcafe => {
+      this.testcafe = testcafe;
+      const runner = testcafe.createRunner();
+      if (this.options.proxy) {
+        runner.useProxy(this.options.proxy);
+      }
+      let runOptions = {
+        debugOnFail: this.options.debugOnFail,
+        skipJsErrors: true
+      };
+      return runner
+        .src(`./${this.testFile}`)
+        .screenshots(`${this.options.report_folder}/screenshots/`, true)
+        .browsers(this.options.browsers)
+        .reporter("spec", `${this.options.report_folder}/report.txt`)
+        .run(runOptions);
+    });
+  }
+
+  /* Close the stream, file and browser/server */
+  close() {
+    debug("close testcafe, close report stream, unlink file");
+    this.testcafe.close();
+    unlinkSync(`./${this.testFile}`);
+  }
 }
-module.exports = Runner
+module.exports = Runner;
--- stepWrapper.js
+++ stepWrapper.js
@@ -1,27 +1,78 @@
-const processTestFnError = require('testcafe/lib/errors/process-test-fn-error')
+const processTestFnError = require("testcafe/lib/errors/process-test-fn-error");
 
-/* Wrapper for the Given, When and Then Cucumber functions, this wrapper adds any error to the TestCafé testrun 
+/* Wrapper for the Given, When and Then Cucumber functions, this wrapper adds any error to the TestCafé testrun
  * since both Cucumber and TestCafé use a catch to find errors in the tests, but Cucumber catches them before TestCafé
  * sees them. This wrapper makes the error also known to TestCafé so that you can also use the TestCafé reporting
-*/
-module.exports = function(fn){
-    const arity = fn.length
-    switch(arity){
-        case 0:
-            return async function(){try {return await fn.apply(this, arguments)} catch(ex){t.testRun.addError(processTestFnError(ex));throw ex}}
-        case 1:
-            return async function(a){try {return await fn.apply(this, arguments)} catch(ex){t.testRun.addError(processTestFnError(ex));throw ex}}
-        case 2:
-            return async function(a, b){try {return await fn.apply(this, arguments)} catch(ex){t.testRun.addError(processTestFnError(ex));throw ex}}
-        case 3:
-            return async function(a, b, c){try {return await fn.apply(this, arguments)} catch(ex){t.testRun.addError(processTestFnError(ex));throw ex}}
-        case 4:
-            return async function(a, b, c, d){try {return await fn.apply(this, arguments)} catch(ex){t.testRun.addError(processTestFnError(ex));throw ex}}
-        case 5:
-            return async function(a, b, c, d, e){try {return await fn.apply(this, arguments)} catch(ex){t.testRun.addError(processTestFnError(ex));throw ex}}
-        case 6:
-            return async function(a, b, c, d, e, f){try {return await fn.apply(this, arguments)} catch(ex){t.testRun.addError(processTestFnError(ex));throw ex}}
-        default:
-            throw Error('PickleCafé only supports a maximum arity of six for the step definitions.')
-    }
-}
+ */
+module.exports = function(fn) {
+  const arity = fn.length;
+  switch (arity) {
+    case 0:
+      return async function() {
+        try {
+          return await fn.apply(this, arguments);
+        } catch (ex) {
+          t.testRun.addError(processTestFnError(ex));
+          throw ex;
+        }
+      };
+    case 1:
+      return async function(a) {
+        try {
+          return await fn.apply(this, arguments);
+        } catch (ex) {
+          t.testRun.addError(processTestFnError(ex));
+          throw ex;
+        }
+      };
+    case 2:
+      return async function(a, b) {
+        try {
+          return await fn.apply(this, arguments);
+        } catch (ex) {
+          t.testRun.addError(processTestFnError(ex));
+          throw ex;
+        }
+      };
+    case 3:
+      return async function(a, b, c) {
+        try {
+          return await fn.apply(this, arguments);
+        } catch (ex) {
+          t.testRun.addError(processTestFnError(ex));
+          throw ex;
+        }
+      };
+    case 4:
+      return async function(a, b, c, d) {
+        try {
+          return await fn.apply(this, arguments);
+        } catch (ex) {
+          t.testRun.addError(processTestFnError(ex));
+          throw ex;
+        }
+      };
+    case 5:
+      return async function(a, b, c, d, e) {
+        try {
+          return await fn.apply(this, arguments);
+        } catch (ex) {
+          t.testRun.addError(processTestFnError(ex));
+          throw ex;
+        }
+      };
+    case 6:
+      return async function(a, b, c, d, e, f) {
+        try {
+          return await fn.apply(this, arguments);
+        } catch (ex) {
+          t.testRun.addError(processTestFnError(ex));
+          throw ex;
+        }
+      };
+    default:
+      throw Error(
+        "PickleCafé only supports a maximum arity of six for the step definitions."
+      );
+  }
+};
